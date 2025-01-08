cc401958056c5f77ca9ee5467175d8ad0ba5d042
Michael Fitoussi <16884890+mifitous@users.noreply.github.com>
2025-01-08 16:25:29 -0500

    Task 8

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
+"""
+server.py
+
+This module handles the main server logic, including routing, request handling, 
+and response generation.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +12,22 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """
+    Processes the default page
+
+    Returns:
+        str: A default html page
+    """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    Processes the emotion data to extract insights.
+
+    Returns:
+        str: A summary of the dominant emotion.
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -26,9 +45,13 @@ def sent_analyzer():
     # Check if the dominant_emotion is None, indicating an error or invalid input
     if dominant_emotion is None:
         return "Invalid text! Please try again!."
-    else:
-        # Return a formatted string with the sentiment label and score
-        return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+
+    # Return a formatted string with the sentiment label and score
+    return (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
